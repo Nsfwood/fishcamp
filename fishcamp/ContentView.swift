e141d91947c8a618ad15d08e63d9902f2e092d04
Alexander Rohrig <me@alexanderrohrig.com>
2020-11-01 12:00:54 -0800

    Initial Commit

---
+++ fishcamp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  fishcamp
+//
+//  Created by Alexander Rohrig on 11/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
